665f8e52c26677e941f373e51eb8f86885329960
Breno Leonhardt Pacheco <brenoleonhardt@gmail.com>
2024-11-11 18:32:25 +0000

    fix: update Makefile

--- config.mk
+++ config.mk
@@ -1,22 +1,11 @@
-# slstatus version
-VERSION = 1.0
+VERSION := 1.0.r$(shell git rev-list --count HEAD).$(shell git rev-parse --short HEAD)
 
-# customize below to fit your system
+X11INC = /usr/include/X11
+X11LIB = /usr/lib
 
-# paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
-
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
-
-# flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\"
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter -Os
 LDFLAGS  = -L$(X11LIB) -s
-# OpenBSD: add -lsndio
-# FreeBSD: add -lkvm -lsndio
 LDLIBS   = -lX11
 
-# compiler and linker
 CC = cc
